5e045b0de7f88d4dc09e110e38d894516f3a2c6c
brinda <brinda.ts@gmail.com>
2014-01-28 16:32:20 -0800

    first check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdPosts::Application.config.secret_key_base = '8f1cdd96dffdceb1cfbdc864fffd3baa3ebc69a4e458bfe5ca7eeb6786615ca0c1aa7e5f0d508b2c87e3564660ad265a1e439baad6b2799fe4bb856a6c36114d'
